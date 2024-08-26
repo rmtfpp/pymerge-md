876318bb1d5a613b7c5e68d19e843062e3aa1f1e
rmtfpp <rmtfpp@gmail.com>
2024-08-26 19:47:29 +0200

    asserion ok

--- pymerge-md.py
+++ pymerge-md.py
@@ -1,6 +1,14 @@
-def main():
-    return 0
+import sys
+import os
+from pathlib import Path
 
+def main():
+
+    path = sys.argv[1]
+    path = Path(path)
+    assert os.path.exists(path) and len([x for x in path.iterdir() if x.is_dir() == False and str(x)[-3:] == ".md"]) != 0, "enter a valid path with a markdown file in it"
+
+    
 
 if __name__ == "__main__":
     main()
